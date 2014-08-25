cd1ce5a1bfc0a7621b470612cd70b61748d83111
Nick White <nickswhite89@gmail.com>
2014-08-25 11:31:02 +0100

    Multiple places & distances
    - Allows for using multiple places for "to"
    - Allows for different distance types
      - Yards
      - KM
      - Metres
      - Miles
      - Feet

--- index.js
+++ index.js
@@ -13,36 +13,121 @@ if (typeof(Number.prototype.toRad) === 'undefined') {
     };
 }
 
-// start and end are objects with latitude and longitude
-//decimals (default 2) is number of decimals in the output
-//return is distance in kilometers.
-exports.getDistance = function (toLat, toLng, fromLat, fromLng, decimals) {
-    var earthRadius,
-        dLat,
-        dLon,
-        lat1,
-        la2,
-        a,
-        c,
-        d;
+exports.distances = {
+    'yards': 6967410,
+    'km': 6371,
+    'miles': 3959,
+    'metres': 6371000,
+    'feet': 20902231
+};
 
+/**
+ * Sort the to object in ascending or descending order if called
+ * @param  {object} toObj      [Going to Lng, Lat and distance]
+ * @param  {number} toObj.lat  [Latitude]
+ * @param  {number} toObj.lng  [Longitutde]
+ * @param  {number} toObj.distance [Distance]
+ * @param  {string} orderBy [Ascending or Descending]
+ * @return {object}         [Object to, in order by distance]
+ */
+exports.distanceSort = function (toObj, orderBy) {
+    toObj.sort(function (a, b) {
+        var a1 = a.distance, b1 = b.distance;
+        if (a1 == b1) { return 0; }
+        return a1> b1 ? 1 : -1;
+    });
+
+    if (orderBy != 'asc') {
+        toObj.reverse();
+    }
+
+    return toObj;
+};
+
+/**
+ * Function which calculates a single distance between two points
+ * @function retrieveDistance
+ * @memberOf geoDistance
+ * @param  {object} data [Created by the run function]
+ * @param  {number} data.earthRadius [Contains the radius]
+ * @param  {number} data.decimals [Contains the amount of decimals]
+ * @param  {number} data.dLat [From minus to Lat radian]
+ * @param  {number} data.dLng [From minus to Lng radian]
+ * @param  {number} data.toLat [To Latitude]
+ * @param  {number} data.toLng [To Longitude]
+ * @param  {number} data.fromLat [From Latitude radian]
+ * @return {number}      [Returns the caculated distance]
+ */
+exports.retrieveDistance = function (data) {
+    var earthRadius = data.earthRadius,
+        decimals = data.decimals,
+        dLat = data.dLat,
+        dLng = data.dLng,
+        toLat = data.toLat,
+        toLng = data.toLng,
+        fromLat = data.fromLat,
+
+        a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+            Math.sin(dLng / 2) * Math.sin(dLng / 2) * Math.cos(toLat) *
+            Math.cos(fromLat),
+        b = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a)),
+        c = earthRadius * b,
+        d = (Math.round(c * Math.pow (10, decimals)) /
+            Math.pow(10, decimals)) * 1000;
+
+    return d;
+};
+
+/**
+ * Function to retrieve the difference in a human readable length such
+ * as km between two difference longitude and latitude points
+ * @function init
+ * @memberOf geoDistance
+ *
+ * @param  {object} from    [Contains the main starting lng/lat point]
+ * @param  {number} from.lat [Latitude]
+ * @param  {number} from.lng [Longitutde]
+ * @param  {array<object>}  to [Can contain multiple end lng/lat points]
+ * @param  {number} to.lat [Latitude]
+ * @param  {number} to.lng [Longitude]
+ *
+ * @return {Object}          [Returns the to object with the distance]
+ */
+exports.getDistance = function (from, to, orderBy, length, decimals) {
     decimals = decimals || 2;
-    earthRadius = 6371; // km
-    lat1 = parseFloat(toLat);
-    lat2 = parseFloat(fromLat);
-    lon1 = parseFloat(toLng);
-    lon2 = parseFloat(fromLng);
+    length = length || 'km';
 
-    dLat = (lat2 - lat1).toRad();
-    dLon = (lon2 - lon1).toRad();
-    lat1 = lat1.toRad();
-    lat2 = lat2.toRad();
+    var fromObj = from,
+        toObj = to.slice(0),
+        earthRadius = this.distances[length],
+        fromLng = parseFloat(fromObj.lng),
+        fromLat = parseFloat(fromObj.lat);
 
-    a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-            Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
-    c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    d = earthRadius * c;
+    fromLat = fromLat.toRad();
 
-    return (Math.round(d * Math.pow(10, decimals)) / Math.pow(10, decimals)) *
-        1000;
+    for (var i = to.length - 1; i >= 0; i--) {
+        var lngLat = toObj[i],
+            toLat = parseFloat(lngLat.lat),
+            toLng = parseFloat(lngLat.lng),
+            dLat = (fromLat - toLat).toRad(),
+            dLng = (fromLng - toLng).toRad(),
+            data = {
+                earthRadius: earthRadius,
+                decimals: decimals,
+                dLat: dLat,
+                dLng: dLng,
+                toLng: toLng,
+                fromLat: fromLat
+            };
+
+        data.toLat = toLat.toRad();
+
+        toObj[i].distance = this.retrieveDistance(data);
+    }
+
+    if (orderBy) {
+        toObj = this.distanceSort(toObj, orderBy);
+    }
+
+    return toObj;
 };
--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,28 +1,163 @@
-var dist = require('../index.js'),
-    whitehouse = {
-        lat: 38.897096,
-        lng: -77.036545
-    },
-    bigBen = {
-        lat: 51.50077,
-        lng: -0.124640
-    };
+var expect = require('expect.js'),
+    geoDist = require('../index.js');
 
-exports['getDistance'] = function (test) {
-    // Returns zero because is the same
-    test.equal(dist.getDistance(whitehouse.lat, whitehouse.lng, whitehouse.lat, whitehouse.lng, 2), 0);
+describe('Get distance', function () {
+    var to = [
+            {
+                lat: -22.853840,
+                lng: 19.882812
+            },
+            {
+                lat: 44.514772,
+                lng: 20.058594
+            }
+        ],
+        from = {
+            lat: 58.114637,
+            lng: -4.375000
+        };
 
-    // Returns 5898770 km
-    test.equal(dist.getDistance(whitehouse.lat, whitehouse.lng, bigBen.lat, bigBen.lng, 2), 5898770);
+    it('should have toRad function on number prototype', function () {
+        var num = 50;
 
-    // Returns 5898770 km with no decimals passed
-    test.equal(dist.getDistance(whitehouse.lat, whitehouse.lng, bigBen.lat, bigBen.lng), 5898770);
+        expect(num.toRad).to.be.an('function');
+        expect(num.toRad()).to.eql(0.8726646259971648);
+    });
 
-    // Check toRad to exist
-    test.equal(Number.toRad);
+    it('should export needed functions and objects', function () {
+        expect(geoDist.distances).to.be.an('object');
+        expect(geoDist.distanceSort).to.be.a('function');
+        expect(geoDist.retrieveDistance).to.be.a('function');
+        expect(geoDist.getDistance).to.be.a('function');
+    });
 
-    // Check toRad should return correctly
-    test.equal(parseFloat(1).toRad(), 0.017453292519943295);
+    describe('Has the correct lengths available', function () {
+        var distances = geoDist.distances;
 
-    test.done();
-};
+        it('should have the circumfrance of the earth in yards', function () {
+            expect(distances.yards).to.eql(6967410);
+        });
+
+        it('should have the circumfrance of the earth in km', function () {
+            expect(distances.km).to.eql(6371);
+        });
+
+        it('should have the circumfrance of the earth in miles', function () {
+            expect(distances.miles).to.eql(3959);
+        });
+
+        it('should have the circumfrance of the earth in metres', function () {
+            expect(distances.metres).to.eql(6371000);
+        });
+
+        it('should have the circumfrance of the earth in feet', function () {
+            expect(distances.feet).to.eql(20902231);
+        });
+    });
+
+    describe('Check ordering of objects', function () {
+        var toOrder = [
+                {
+                    distance: 5141690
+                },
+                {
+                    distance: 3265390
+                },
+                {
+                    distance: 5357190
+                },
+                {
+                    distance: 5357190
+                }
+            ],
+            toBe = [
+                {
+                    distance: 5357190
+                },
+                {
+                    distance: 5141690
+                },
+                {
+                    distance: 3265390
+                }
+            ];
+
+        it('Should order ascending', function () {
+            var ordered = geoDist.distanceSort(toOrder, 'asc');
+
+            expect(ordered[0].distance).to.be(toBe[2].distance);
+            expect(ordered[1].distance).to.be(toBe[1].distance);
+            expect(ordered[2].distance).to.be(toBe[0].distance);
+        });
+
+        it('Should order descending', function () {
+            var ordered = geoDist.distanceSort(toOrder, 'desc');
+
+            expect(ordered[0].distance).to.be(toBe[0].distance);
+            expect(ordered[1].distance).to.be(toBe[0].distance);
+            expect(ordered[2].distance).to.be(toBe[1].distance);
+        });
+
+    });
+
+    it('Should return the distance', function () {
+        var data = {
+                'earthRadius': 6967410,
+                'decimals': 2,
+                'dLat': -0.7592266063998,
+                'dLng': -0.4264466633955314,
+                'toLng': 20.058594,
+                'fromLat': 1.0142917592513199,
+                'toLat': 0.7769293371745812
+            },
+            distance = geoDist.retrieveDistance(data);
+
+        expect(distance).to.be(5623014900);
+    });
+
+    describe('Init function retrieves the distances', function () {
+
+        it('Doesn\'t need decimals or length set', function () {
+            var result = geoDist.getDistance(from, to);
+
+            expect(result[0].distance).to.be(3265390);
+            expect(result[1].distance).to.be(5141690);
+        });
+
+        it('Takes a lng and lat using yards', function () {
+            var resultYards = geoDist.getDistance(from, to, 'asc', 'yards', 2);
+
+            expect(resultYards[0].distance).to.be(3571078080);
+            expect(resultYards[1].distance).to.be(5623014900);
+        });
+
+        it('Takes a lng and lat using km', function () {
+            var resultKm = geoDist.getDistance(from, to, 'asc', 'km', 2);
+
+            expect(resultKm[0].distance).to.be(3265390);
+            expect(resultKm[1].distance).to.be(5141690);
+        });
+
+        it('Takes a lng and lat using miles', function () {
+            var resultMiles = geoDist.getDistance(from, to, 'asc', 'miles', 2);
+
+            expect(resultMiles[0].distance).to.be(2029150);
+            expect(resultMiles[1].distance).to.be(3195090);
+        });
+
+        it('Takes a lng and lat using metres', function () {
+            var resultMetres = geoDist.getDistance(from, to, 'asc', 'metres', 2);
+
+            expect(resultMetres[0].distance).to.be(3265393950);
+            expect(resultMetres[1].distance).to.be(5141685060);
+        });
+
+        it('Takes a lng and lat using feet', function () {
+            var resultFeet = geoDist.getDistance(from, to, 'asc', 'feet', 2);
+
+            expect(resultFeet[0].distance).to.be(10713234760);
+            expect(resultFeet[1].distance).to.be(16869045500);
+        });
+    });
+
+});
